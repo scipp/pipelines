6993c33ae1f1060b53ea1ff40685efc87e74f3df
Neil Vaytet <neil.vaytet@esss.se>
2021-05-20 14:03:53 +0200

    revert removal of python

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,5 +1,3 @@
 if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX=%cd%\pipelines_install & call tools\make_and_install.bat
 
-dir /s /b %INSTALL_PREFIX%
-
 move %INSTALL_PREFIX%\pipelines-test %CONDA_PREFIX%\lib\
