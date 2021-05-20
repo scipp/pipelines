b702688f91ddaab1a07c61cf0623ff9fab389079
Neil Vaytet <neil.vaytet@esss.se>
2021-05-20 13:51:24 +0200

    view dir contents

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,3 +1,5 @@
 if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX=%cd%\pipelines_install & call tools\make_and_install.bat
 
+dir /s /b %INSTALL_PREFIX%
+
 move %INSTALL_PREFIX%\pipelines-test %CONDA_PREFIX%\lib\
