6685c7eecc6a54cba8579aafe4c728ce64636ab7
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-06-10 15:28:47 +0200

    Add builder arg to build_docs

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -9,13 +9,16 @@ import sys
 parser = argparse.ArgumentParser(description='Build doc pages with sphinx')
 parser.add_argument('--prefix', default='build')
 parser.add_argument('--work_dir', default='.doctrees')
+parser.add_argument('--builder', default='html')
 
 if __name__ == '__main__':
 
     args = parser.parse_args()
 
     # Build the docs with sphinx-build
-    status = subprocess.check_call(
-        ['sphinx-build', '-d', args.work_dir, '.', args.prefix],
-        stderr=subprocess.STDOUT,
-        shell=sys.platform == "win32")
+    status = subprocess.check_call([
+        'sphinx-build', '-b', args.builder, '-d', args.work_dir, '.',
+        args.prefix
+    ],
+                                   stderr=subprocess.STDOUT,
+                                   shell=sys.platform == "win32")
