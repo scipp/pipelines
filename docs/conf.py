0be02e6c805f2427e2f315404f3548443ff3ae8b
Neil Vaytet <neil.vaytet@esss.se>
2021-08-27 16:15:52 +0200

    flake8

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
-from scippbuildtools.sphinxconf import *  # noqa: F403
+from scippbuildtools.sphinxconf import *  # noqa: F401, F403
 
 project = u'pipelines-test'
 
