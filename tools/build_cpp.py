925abd71c0364ebc54553462bf0b134ddf0380d4
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-09-15 14:10:36 +0100

    Try just the minimum OSX_TARGET

--- tools/build_cpp.py
+++ tools/build_cpp.py
@@ -59,15 +59,7 @@ def main(prefix='install', build_dir='build', source_dir='.', caching=False):
         cmake_flags.update({'-DCMAKE_INTERPROCEDURAL_OPTIMIZATION': 'OFF'})
         osxversion = os.environ.get('OSX_VERSION')
         if osxversion is not None:
-            cmake_flags.update({
-                '-DCMAKE_OSX_DEPLOYMENT_TARGET':
-                osxversion,
-                '-DCMAKE_OSX_SYSROOT':
-                os.path.join('/Applications', 'Xcode.app', 'Contents',
-                             'Developer', 'Platforms', 'MacOSX.platform',
-                             'Developer', 'SDKs',
-                             'MacOSX{}.sdk'.format(osxversion))
-            })
+            cmake_flags.update({'-DCMAKE_OSX_DEPLOYMENT_TARGET': '10.14'})
 
     if platform == 'win32':
         cmake_flags.update({'-G': 'Visual Studio 16 2019', '-A': 'x64'})
